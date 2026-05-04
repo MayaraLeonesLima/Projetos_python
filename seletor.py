ea127d1aca9bdeba521dc3c762063027b38f6491
MayaraLeonesLima <maynotebook992@gmail.com>
2026-05-04 09:38:04 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de Caracteres--
+letra = input("Digite a Letra")
+if letra == "a":
+ print("Voce escolheu a Letra A")
+elif letra == "b"
+ print("Voce escolheu a letra B")
+else:
+ print("Essa opção não está disponivel")

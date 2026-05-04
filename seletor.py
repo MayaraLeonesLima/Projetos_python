3772e406e9171e3c9d00326651af10f91d3d17fd
MayaraLeonesLima <maynotebook992@gmail.com>
2026-05-04 14:09:13 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--Seletor de Caracteres--
-letra = input("Digite a Letra")
-if letra == "a":
- print("Voce escolheu a Letra A")
-elif letra == "b"
- print("Voce escolheu a letra B")
+print("Sistema Seletor de Caracteres🔠")
+print("Escolha uma Letra e veja se está disponivel👀")
+letra = input("Digite a Letra: ").upper()
+if letra == "A":
+ print("Voce escolheu a Letra A ✅")
+elif letra == "B":
+ print("Voce escolheu a letra B ✅")
 else:
- print("Essa opção não está disponivel")
+ print("Essa opção não está disponivel ❌")

e64b30e3f31da96041ee1757504eabff692a3013
MayaraLeonesLima <maynotebook992@gmail.com>
2026-05-18 14:23:07 +0000

    v1

---
+++ quizz.py
@@ -0,0 +1,17 @@
+print("Quiz Futebol⚽")
+
+# pergunta 1
+print("1- Quem ganhou a copa de 2002🏆?/n a- alemanha/n b- brasil/n c- argentina")
+r1 = input("resposta: ")
+
+if r1 == "b":
+    print("acertou🥳/n")
+else:
+    print("Errou, era Brasil❌/n")
+
+# pergunta 2
+print("2- Qual jogador é o maior artilheiro da historia da Liga dos Campeões da UEFA?/n a-Mbappé b- Haaland/n c- Cristiano Ronaldo")
+r2 = input("resposta: ")
+
+if r2 == "c" :
+    print(Acertou)

6440e31a9cf99b31bbc2f588b3b7dffbc0967db3
MayaraLeonesLima <maynotebook992@gmail.com>
2026-05-25 12:40:57 +0000

    v1

---
+++ cabeçalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamando a Função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvido no Codespace')
+    print('=' * 30)
+
+    ### Chamando a função
+    exibir_cabecalho()
+    print('Menu Principal:')

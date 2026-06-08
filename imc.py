f7d76b681fe2e902485f76be8f388494fb91ca70
MayaraLeonesLima <maynotebook992@gmail.com>
2026-06-08 15:32:49 +0000

    v2

--- imc.py
+++ imc.py
@@ -11,4 +11,16 @@ def classificacao_imc(resultado):
         return "PESO NORMAL"
     #etapa 3 - Mensagem de retorno
     def mensagem(status):
-        if status ==
+        if status == "ACIMA DO PESO":
+            return "PROCURE UM MÉDICO!⚠️"
+        else:
+            return "peso ok"
+#etapa 4 - integração do código
+valor_peso = float(input("digite o seu peso"))
+valor_altura = float(input("Digite a sua altura"))
+
+valor_imc = calc_imc(valor_peso,valor_altura)
+resultado_imc = classificar_imc(valor_imc)
+saida = mensagem(resultado_imc)
+
+print(f"n/")
